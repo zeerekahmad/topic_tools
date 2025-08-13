5d0231ee8c2ed6f4e8bff21f15a64eca8e71582f
Zeerek <zeerek@polymathrobotics.com>
2025-08-13 18:51:59 +0000

    Add qos overrides to humble

--- topic_tools/src/tool_base_node.cpp
+++ topic_tools/src/tool_base_node.cpp
@@ -30,14 +30,35 @@ ToolBaseNode::ToolBaseNode(const std::string & node_name, const rclcpp::NodeOpti
 void ToolBaseNode::make_subscribe_unsubscribe_decisions()
 {
   if (auto source_info = try_discover_source()) {
+    // publisher exists already but needs changing if output_topic_ changes
+    if (pub_ &&
+      pub_->get_topic_name() !=
+      get_node_topics_interface()->resolve_topic_name(output_topic_))
+    {
+      pub_.reset();
+    }
+
+
     // always relay same topic type and QoS profile as the first available source
     if (!topic_type_ || !qos_profile_ || *topic_type_ != source_info->first ||
       *qos_profile_ != source_info->second || !pub_)
-    {
+      {
+      rclcpp::PublisherOptions options;
+      options.qos_overriding_options =  rclcpp::QosOverridingOptions
+      {
+        rclcpp::QosPolicyKind::Deadline,
+        rclcpp::QosPolicyKind::Durability,
+        rclcpp::QosPolicyKind::History,
+        rclcpp::QosPolicyKind::Depth,
+        rclcpp::QosPolicyKind::Lifespan,
+        rclcpp::QosPolicyKind::Liveliness,
+        rclcpp::QosPolicyKind::LivelinessLeaseDuration,
+        rclcpp::QosPolicyKind::Reliability,
+      };
       topic_type_ = source_info->first;
       qos_profile_ = source_info->second;
       std::scoped_lock lock(pub_mutex_);
-      pub_ = this->create_generic_publisher(output_topic_, *topic_type_, *qos_profile_);
+      pub_ = this->create_generic_publisher(output_topic_, *topic_type_, *qos_profile_, options);
     }
     // at this point it is certain that our publisher exists
 
